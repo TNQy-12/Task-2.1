407b2ba367e30e90326eeb35fc6b697e9b31afcd
TNQy-12 <165918821+TNQy-12@users.noreply.github.com>
2024-04-05 00:57:04 +0700

    -UPload Task 2.1
    None

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/1147679ae7.js" crossorigin="=anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Background Image Example</title>
+    <link rel="images" href="images.css">
+    <title>Shop yêu thương</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <img src="" alt="SAMI">
+        </div>
+        <div class="menu">
+            <li><a href="">NỮ</a></li>
+            <ul class="sub-menu">
+                <li><a href="">Hàng mới về</a></li>
+                <li><a href="">Collection</a></li>
+                <li><a href="">Áo</a>
+                    <ul>
+                        <li><a href="">Áo sơ mi</a></li>
+                        <li><a href="">Áo thun</a></li>
+                        <li><a href="">Áo khoác</a></li>
+                        <li><a href="">Áo len</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Quần</a>
+                    <ul>
+                        <li><a href="">Quần dài</a></li>
+                        <li><a href="">Quần lửng</a></li>
+                        <li><a href="">Quần kaki </a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <li><a href="">NAM</a></li>
+            <ul class="sub-menu">
+                <li><a href="">Hàng mới về</a></li>
+                <li><a href="">Collection</a></li>
+                <li><a href="">Áo</a>
+                    <ul>
+                        <li><a href="">Áo khoác</a></li>
+                        <li><a href="">Áo polo</a></li>
+                        <li><a href="">Áo thun</a></li>
+                        <li><a href="">Áo sơ mi</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Quần</a>
+                    <ul>
+                        <li><a href="">Quần jean</a></li>
+                        <li><a href="">Quần lửng</a></li>
+                        <li><a href="">Quần kaki </a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <li><a href="">TRẺ EM</a></li>
+            <ul class="sub-menu">
+                <li><a href="">Hàng mới về</a></li>
+                <li><a href="">Collection</a></li>
+                <li><a href="">Áo</a>
+                    <ul>
+                        <li><a href="">Áo babi</a></li>
+                        <li><a href="">Áo con vịt</a></li>
+                        <li><a href="">Áo ba lỗ</a></li>
+                        <li><a href="">Áo sơ mi</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Quần</a>
+                    <ul>
+                        <li><a href="">Quần babi</a></li>
+                        <li><a href="">Quần con vịt</a></li>
+                        <li><a href="">Quần đùi </a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <li><a href="">SALE</a></li>
+            <ul class="sub-menu">
+                <li><a href="">Hàng mới về</a></li>
+                <li><a href="">Collection</a></li>
+                <li><a href="">Áo</a>
+                    <ul>
+                        <li><a href="">Áo babi</a></li>
+                        <li><a href="">Áo con vịt</a></li>
+                        <li><a href="">Áo ba lỗ</a></li>
+                        <li><a href="">Áo sơ mi</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Quần</a>
+                    <ul>
+                        <li><a href="">Quần babi</a></li>
+                        <li><a href="">Quần con vịt</a></li>
+                        <li><a href="">Quần đùi </a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <li><a href="">KHẨU TRANG</a></li>
+            <ul class="sub-menu">
+                <li><a href="">Hàng mới về</a></li>
+                <li><a href="">Collection</a></li>
+                <li><a href="">Loại</a>
+                    <ul>
+                        <li><a href="">khẩu trang 2d</a></li>
+                        <li><a href="">khẩu trang 3d</a></li>
+                        <li><a href="">khẩu trang 4d</a></li>
+                        <li><a href="">khẩu trang 5d</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Màu</a>
+                    <ul>
+                        <li><a href="">Màu xanh</a></li>
+                        <li><a href="">Màu </a></li>
+                        <li><a href="">Màu trắng </a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <li><a href="">BST</a></li>
+            <li><a href="">THÔNG TIN</a></li>
+        </div>
+        <div class="others">
+            <li><input placeholder="Tìm Kiếm" type="text"><i class="fas fa-search"></i></li>
+            <li> <a class="fa fa-paw" href=""></a></li>
+            <li> <a class="fa fa-user" href=""></a></li>
+            <li> <a class="fa fa-shopping-bag" href=""></a></li>
+
+        </div>
+    </header>
+    <section id="slider">
+        <div class="slider-container">
+            <img src="https://img.thuthuat123.com/uploads/2019/07/16/hinh-nen-4k-cho-windows-10_100820928.jpg" width="1500" height="1000">
+        </div>
+    </section>
+    <div class="wrapper">
+        <form action="">
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Username" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="wrapper">
+                <form action="">
+                    <div class="input-box">
+                        <input type="password" placeholder="password" required>
+                        <i class='bx bxs-lock-alt'></i>
+                    </div>
+
+                    <div class="remember-forgot">
+                        <label><input type="checkbox">remember me </label>
+                        <a href="#">forgot password?</a>
+                    </div>
+                    <button type="submit" class="btn">Login</button>
+                    <div class="register-link">
+                        <p>Don't have an account? <a href="#">register</a></p>
+                    </div>
+                </form>
+                <form action="">
+                    <h1>Submit</h1>
+                    <div class="input-box">
+                        <input type="text" placeholder="Username" required>
+                        <i class='bx bxs-user'></i>
+                    </div>
+                    <div class="wrapper">
+                        <form action="">
+                            <div class="input-box">
+                                <input type="password" placeholder="password" required>
+                                <i class='bx bxs-lock-alt'></i>
+                            </div>
+
+                            <div class="remember-forgot">
+                                <label><input type="checkbox">remember me </label>
+                                <a href="#">forgot password?</a>
+                            </div>
+                            <button type="submit" class="btn">Login</button>
+                            <div class="register-link">
+                                <p>Don't have an account? <a href="#">register</a></p>
+                            </div>
+                        </form>
+
+                    </div>
+
+</body>
+
+</html>
